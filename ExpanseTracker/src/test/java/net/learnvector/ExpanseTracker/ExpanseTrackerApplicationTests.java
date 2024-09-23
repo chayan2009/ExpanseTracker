3fdd6b3470233655a0cdc3ea470de3acc248a544
chayan <chayanchowdhury@gmail.com>
2024-09-23 23:32:54 +0530

    initial commit

---
+++ ExpanseTracker/src/test/java/net/learnvector/ExpanseTracker/ExpanseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.learnvector.ExpanseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpanseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
